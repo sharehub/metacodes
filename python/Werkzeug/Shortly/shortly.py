3c1cc5f54a85ecf8622cfce237d5ece7d2939f3a
Leon <dontmailme@notexist.com>
2013-11-22 11:10:05 +0800

    update

---
+++ python/Werkzeug/Shortly/shortly.py
@@ -0,0 +1,39 @@
+import os
+from werkzeug.wrappers import Request, Response
+from werkzeug.routing import Map, Rule
+from werkzeug.exceptions import HTTPException, NotFound
+from werkzeug.wsgi import SharedDataMiddleware
+from werkzeug.utils import redirect
+
+class Shortly(object):
+
+    def __init__(self, config):
+	pass
+
+    def dispatch_request(self, request):
+        return Response('Hello World!')
+
+    def wsgi_app(self, environ, start_response):
+        request = Request(environ)
+        response = self.dispatch_request(request)
+        return response(environ, start_response)
+
+    def __call__(self, environ, start_response):
+        return self.wsgi_app(environ, start_response)
+
+
+def create_app(redis_host='localhost', redis_port=6379, with_static=True):
+    app = Shortly({
+        'redis_host':       redis_host,
+        'redis_port':       redis_port
+    })
+    if with_static:
+        app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+            '/static':  os.path.join(os.path.dirname(__file__), 'static')
+        })
+    return app
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    app = create_app()
+    run_simple('0.0.0.0', 5000, app, use_debugger=True, use_reloader=True)
